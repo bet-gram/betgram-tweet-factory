6de03edb2002f488442eb5d56b3d09e58a151e84
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2015-11-05 23:04:25 -0500

    Update tweet-factory

--- app/tweet-factory.py
+++ app/tweet-factory.py
@@ -1,64 +1,52 @@
-#Import the necessary methods from tweepy library
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy import Cursor
-from tweepy import API
-import json
+#Tweepy wrapper imports
+import tweepy
+from tweepy.auth import OAuthHandler
 
-#Variables that contains the user credentials to access Twitter API 
-access_token = "716680344-atp29IPuiLSiXR6vOHcEMk78tzXKDhy9uWMXm7Uy"
-access_token_secret = "IIEmIHrhtrY5EGnpnx1zwQmipoT6u1bgCjjLFRT58wdvg"
-consumer_key = "kHIJJ90oKRF6Svbf8UnYTRzrz"
-consumer_secret = "zwc65T4hSA4lFk3eQSOp19BRCUvqtqNkOMx43xjX87X29hAkTs"
+#Twitter API authentication fields
+CONSUMER_KEY = "Bk2RrrGJIx3z3R3NzWXk2V0g0"
+CONSUMER_SECRET = "Q5QPiTiapz1GewGV2aeLWoT8IoWXhjBFrC9bsQRvjOjpTSacPK"
+TOKEN_KEY = "435217839-v08vYEBqddJQYHmH42ih269a6QsY0G4SozqEbs4P"
+TOKEN_SECRET = "aYREePRSAlWpPyBQvs3VOYbHnt8w2UZCWyTj21yGzQGzW"
 
-tweets = []
+#Hash to store squads social information
+screen_names = {}
 
+#Procedure to initialize hash values
+def init_hash(init):
+    screen_names["ManUtd"] = init
+    screen_names["SwansOfficial"] = init
+    screen_names["BurnleyOfficial"] = init
+    screen_names["WBAFCofficial"] = init
+    screen_names["whufc_official"] = init
+    screen_names["LCFC"] = init
+    screen_names["SpursOfficial"] = init
+    screen_names["HullCity"] = init
+    screen_names["CPFC"] = init
+    screen_names["QPRFC"] = init
+    screen_names["SouthamptonFC"] = init
+    screen_names["stokecity"] = init
+    screen_names["NUFC"] = init
+    screen_names["Arsenal"] = init
+    screen_names["SunderlandAFC"] = init
+    screen_names["AVFCOfficial"] = init
+    screen_names["ChelseaFC"] = init
+    screen_names["LFC"] = init
+    screen_names["Everton"] = init
+    screen_names["MCFC"] = init
 
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
-
-    def __init__(self):
-        self.count = 0
-
-    def on_data(self, data):
-        if self.count == 10:
-            return False
-        try:
-            self.count += 1         
-            tweet = json.loads(data)
-            place = tweet['text']
-            if place:
-                
-                tweets.append(tweet)
-            return True
-        except:
-            return True
-
-        
-
-    def on_error(self, status):
-        print status
-
+def update_hash(screen_name):
+    tweets = api.user_timeline(screen_name = screen_name, count = 50)
+    print screen_name
+    for tweet in tweets:
+        print tweet.retweet_count
 
 if __name__ == '__main__':
-
     #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
-    api = API(auth)
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['arsenal'])
-
-    #l = Cursor(api.search, q='cricket', geocode="-22.9122,-43.2302,1km").items(10)
-
-    #l = api.search(q="arsenal", geocode='51.5072,-0.1275,10000000km', count=100)
-    #print l[-1]
-
-    for tweet in tweets:
-        print tweet['text']
-        print 
-        #print tweet['user']['time_zone']
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(TOKEN_KEY, TOKEN_SECRET)
+    api = tweepy.API(auth)
+    #Initialize hash
+    init_hash(0)
+    #Update hash
+    for screen_name in screen_names:
+        update_hash(screen_name)
